1576ed97447e115c70686ec5399b6927835ce61e
TawhidulIslamRefat <tirefat544@gamil.com>
2025-11-26 10:25:21 +0600

    Added Navbar and Foooter

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,84 @@
-import React from 'react';
+"use client";
+import Link from "next/link";
 
 const Navbar = () => {
-    return (
-        <div>
-            
+  const links = (
+    <>
+      <li className="text-lg font-medium">
+        <Link href="/">Home</Link>
+      </li>
+      <li className="text-lg font-medium">
+        <Link href="/products">All Products</Link>
+      </li>
+      <li className="text-lg font-medium">
+        <Link href="/add-product">Add Products</Link>
+      </li>
+      <li className="text-lg font-medium">
+        <Link href="/manage-products">Manage Products</Link>
+      </li>
+    </>
+  );
+
+  return (
+    <div className="bg-base-300 shadow-2xs">
+      <div className="navbar w-11/12 mx-auto">
+        <div className="navbar-start">
+          {/* Mobile Dropdown */}
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            </div>
+
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box mt-3 w-52 p-2 shadow"
+            >
+              {links}
+            </ul>
+          </div>
+
+          <Link href="/" className="text-3xl font-semibold text-[#A86111]">
+            E-Dokan
+          </Link>
         </div>
-    );
+
+        {/* Desktop Menu */}
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{links}</ul>
+        </div>
+
+        <div className="navbar-end space-x-4">
+          <Link
+            href="/login"
+            className="text-white bg-[#A76111] text-lg font-medium px-4 py-2 rounded-sm 
+             hover:bg-black transform hover:scale-105 duration-300"
+          >
+            Login
+          </Link>
+          <Link
+            href="/Register"
+            className="text-white bg-[#A76111] text-lg font-medium px-4 py-2 rounded-sm 
+             hover:bg-black transform hover:scale-105 duration-300"
+          >
+            Register
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/Components/Navbar/Navbar";
+import Footer from "@/Components/Footer/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,11 +20,15 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="light">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Navbar />
+
+        <main className="grow">{children}</main>
+
+        <Footer />
       </body>
     </html>
   );
